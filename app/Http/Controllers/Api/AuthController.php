682691bc36ba1eaca39dc7ea6c981b6c71ea04b9
Mtblnt01 <balintmate963@gmail.com>
2026-01-08 11:17:33 +0100

    Authcontroller regist kesz

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,39 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Str;
 
 class AuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        try {
+            $validated = $request->validate([
+                'name' => 'required|string|max:255',
+                'email' => 'required|string|email|unique:users',
+                'password' => 'required|string|min:6',
+                'phone' => 'nullable|string|max:20',
+            ]);
+        } catch (ValidationException $e) {
+            return response()->json([
+                'message' => 'Failed to register user',
+                'errors' => $e->errors()
+            ], 422);
+        }
+        $validated['password'] = Hash::make($validated['password']);
+        $validated['remember_token'] = \Str::random(10);
+
+        $user = User::create($validated);
+        return response()->json([
+            'message' => 'User created successfully',
+            'user' => [
+                'id' => $user->id,
+                'name' => $user->name,
+                'email' => $user->email,
+                'phone' => $user->phone,
+            ]
+        ], 201);
+    }
 }
